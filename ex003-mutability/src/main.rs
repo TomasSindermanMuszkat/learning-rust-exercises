7c974f09f8950a2d044aedd4fa4a941f59a0c5f4
The25thBam <44045622+The25thBam@users.noreply.github.com>
2020-11-13 21:11:45 -0300

    Add files via upload

---
+++ ex003-mutability/src/main.rs
@@ -0,0 +1,25 @@
+fn main(){
+    let mut x = 1000;
+    let y = "Programming";
+    println!("x:{}\ny:{}", x, y);
+    x = 1100;
+    println!("x:{}\ny:{}", x, y);
+}
+
+// course
+/* fn main2(){
+    // declare a mutable variable `x`
+    let mut x = 1000;
+    // declare a variable `y`
+    let y="Programming";
+    // print output of `x`
+    println!("x:{}", x);
+    // print output of `y`
+    println!("y:{}", y);
+    // update x
+    x = 1100;
+    // print output of `x`
+    println!("x:{}", x);
+    // print output of `y`
+    println!("y:{}", y);
+} */
