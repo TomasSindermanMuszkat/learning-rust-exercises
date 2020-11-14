9a16a3edbd02be682a8bae417170a0043af52969
The25thBam <44045622+The25thBam@users.noreply.github.com>
2020-11-13 21:13:20 -0300

    Add files via upload

---
+++ ex004-arrays/src/main.rs
@@ -0,0 +1,10 @@
+// explicit
+fn main() {
+    let evens:[i8;6] = [0, 2, 4, 6, 8, 10];
+    println!("{},{},{},{},{},{}", evens[0],evens[1],evens[2],evens[3],evens[4],evens[5]);
+}
+// implicit
+fn implicit() {
+    let evens = [0, 2, 4, 6, 8, 10];
+    println!("{},{},{},{},{},{}", evens[0],evens[1],evens[2],evens[3],evens[4],evens[5]);
+}
